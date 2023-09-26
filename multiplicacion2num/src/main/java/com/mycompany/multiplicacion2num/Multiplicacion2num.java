22e039055e9f0fb6167ea9b63eb0b8da34a4f60e
DIEGOBAL1010 <145385058+DIEGOBAL1010@users.noreply.github.com>
2023-09-26 09:22:52 -0600

    Add files via upload

---
+++ multiplicacion2num/src/main/java/com/mycompany/multiplicacion2num/Multiplicacion2num.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.multiplicacion2num;
+
+/**
+ *
+ * @author User
+ */
+public class Multiplicacion2num {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
